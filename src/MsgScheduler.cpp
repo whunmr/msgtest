9d8c319feef51efc476bb284113beed68a7b2f83
whunmr <whunmr@gmail.com>
2017-03-14 10:08:50 +0800

    refactor

--- src/MsgScheduler.cpp
+++ src/MsgScheduler.cpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <msgtest/listener/CollectLogTestListener.h>
 #include <msgtest/MsgMocker.h>
+#include <cstring>
+#include <list>
+#include <memory>
 
 MSGTEST_NS_START
 
@@ -16,7 +19,21 @@ MSGTEST_NS_START
         }
     }
 
-    void msgtest_dispatch_test_msg(ActorId from, ActorId to, MsgId msgId, const void *payload, size_t len) {
+    struct MsgInfo {
+        MsgInfo(ActorId from, ActorId to, MsgId msgId, const void *payload, size_t len)
+            : from_(from), to_(to), msgId_(msgId), len_(len), payload_(malloc(len), ::free) {
+            memcpy(payload_.get(), payload, len);
+        }
+        ActorId from_;
+        ActorId to_;
+        MsgId msgId_;
+        size_t len_;
+        std::shared_ptr<void> payload_;
+    };
+
+    std::list<MsgInfo> msgQ;
+
+    void call_msg_proc(ActorId from, ActorId to, MsgId msgId, const void *payload, size_t len) {
         g_msgtest_trace_msg_probe_func(from, to, msgId, payload, len);
         g_collect_msg_log_func(from, to, msgId, payload, len);
 
@@ -26,8 +43,22 @@ MSGTEST_NS_START
         }
     }
 
+    void msgtest_dispatch_test_msg(ActorId from, ActorId to, MsgId msgId, const void *payload, size_t len) {
+        MsgInfo msg = MsgInfo(from, to, msgId, payload, len);
+        msgQ.push_back(msg);
+    }
+
+    void MsgScheduler::procAllBufferedMsg() {
+        while (!msgQ.empty()) {
+            const MsgInfo& msg = msgQ.front();
+            call_msg_proc(msg.from_, msg.to_, msg.msgId_, msg.payload_.get(), msg.len_);
+            msgQ.pop_front();
+        }
+    }
+
     void MsgScheduler::scheduleMsg(ActorId from, ActorId to, MsgId msgId, const void *payload, size_t len) {
         msgtest_dispatch_test_msg(from, to, msgId, payload, len);
+        procAllBufferedMsg();
     }
 
     void MsgScheduler::registerMsgProcFunc(ActorId actorId, MsgProcFunc f) {
